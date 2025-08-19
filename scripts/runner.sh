0dc2de7967e02a764e7d6824078341be92d841b7
Rodrigo Machado <machadorodrigo987@gmail.com>
2025-08-19 23:30:05 +0100

    definitive structure, starting python work

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,11 +1,26 @@
 #! /bin/bash
 
-threads_num=[1, 2, 4, 8, 12, 16, 32]
+threads_num=(1 2 4 8 12 16 32) # array of threads to test
 
-dataset="Dataset2"
+read -p "Enter the .jpeg directory: " dataset_dir
+if [ -z "$dataset_dir" ]; then # if string is "zero", set default
+    dataset_dir="datasets/Dataset1"
+fi
 
-mode="-name"
+read -p "Enter the mode (-size or -name): " mode
+if [ -z "$mode" ]; then # if string is "zero", set default
+    mode="-name"
+fi
 
-for num in threads_num:
-    ./build/photo-old $dataset $num $mode
 
+for num in "${threads_num[@]}"; do
+    echo -e "Starting $dataset_dir work with $num threads and $mode\n"
+
+    ./build/photo-old "$dataset_dir" "$num" "$mode"
+
+    rm -rf "$dataset_dir/old-photos"
+
+    echo -e "\nProcessing completed for $dataset_dir with $num threads and $mode\n\n\n"
+done
+
+echo -e "\n\nAll processing completed for $dataset_dir and $mode"
